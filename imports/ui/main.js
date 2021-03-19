c6c78a1867002b0a7680d131825b9ad2fefdc0c1
Marco Tribuzio <marco.tribuz@gmail.com>
2021-03-19 12:23:09 +0100

    Second Commit

---
+++ imports/ui/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import './plugins/tracker'
+import App from "./App";
+import router from "./plugins/router";
+import vuetify from "./plugins/vuetify";
+
+Meteor.startup(() => {
+  new Vue({
+    el: '#app',
+    router: router,
+    vuetify: vuetify,
+    ...App,
+  })
+})
