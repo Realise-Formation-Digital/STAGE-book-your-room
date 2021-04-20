0b45b60623ce4c88ab638577c4d8d0034e8e8a7a
Marco Tribuzio <marco.tribuz@gmail.com>
2021-04-20 16:45:37 +0200

    Bugfix Buildings

--- imports/api/methods/Buildings.js
+++ imports/api/methods/Buildings.js
@@ -1,23 +1,21 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import Building from '../collections/Buildings.js';
+import Rooms from "../collections/Rooms";
 
 Meteor.methods({
 
     /**
      * Insert a building to the database
-     * @param {String} id_building - the building's ID
      * @param {String} building_name - the building's name
      * @param {String} building_adress - the building's adress
      * @returns  
      */
-    'insert_building' (id_building, building_name, building_adress) {
-        check(id_building, String);
+    'insert_building' (building_name, building_adress) {
         check(building_name, String);
         check(building_adress, String);
 
         return Building.insert({
-            'id_building': id_building,
             'Building_Name': building_name,
             'Building_Adress': building_adress,
         });
@@ -33,12 +31,12 @@ Meteor.methods({
      */
     'edit_building' (id_building, building_name, building_adress) {
         check(id_building, String);
-        check(bulding_name, String);
+        check(building_name, String);
         check(building_adress, String);
 
         return Building.update({'_id': id_building}, {
             'Building_Name': building_name,
-            '_Equipement': room_equipement,
+            'Building_Adress': building_adress,
         });
 
     },
@@ -50,12 +48,12 @@ Meteor.methods({
      */
     'delete_building' (id_building) {
         check(id_building, String);
- 
+
+        Rooms.remove({'idBuilding': id_building})
 
         return Building.remove({
             '_id': id_building
         });
-
     },
 
 })
